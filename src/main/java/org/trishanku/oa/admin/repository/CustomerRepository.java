f496ccf2af81d8774c7b801f69cbe28fbd97ce1c
Ravikanth <ravikanthm@gmail.com>
2021-09-08 19:29:20 +0400

    Implemented get customer by customer id closes #4

--- src/main/java/org/trishanku/oa/admin/repository/CustomerRepository.java
+++ src/main/java/org/trishanku/oa/admin/repository/CustomerRepository.java
@@ -3,8 +3,11 @@ package org.trishanku.oa.admin.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.trishanku.oa.admin.entity.Customer;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CustomerRepository extends JpaRepository<Customer, UUID> {
+
+    Optional<Customer> findByCustomerId(String customerId);
 
 }
