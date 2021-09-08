03c23871ada9c73dfb0362a70dd0ed36db1fa5d3
Ravikanth <ravikanthm@gmail.com>
2021-09-08 14:52:59 +0400

    With customer entity and customer model. closes #1 and #2

---
+++ src/main/java/org/trishanku/oa/admin/entity/Customer.java
@@ -0,0 +1,51 @@
+package org.trishanku.oa.admin.entity;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity(name="CUSTOMER_TABLE")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Customer {
+    @Id
+    @Column(name = "SYSTEM_ID")
+    private UUID uuid;
+    @Column(name = "CUSTOMER_ID")
+    private String customerId;
+    @Column(name = "NAME")
+    private String name;
+    @Column(name = "ADDRESS_LINE1")
+    private String addressLine1;
+    @Column(name = "ADDRESS_LINE2")
+    private String addressLine2;
+    @Column(name = "ADDRESS_LINE3")
+    private String addressLine3;
+    @Column(name = "PO_BOX")
+    private String poBox;
+    @Column(name = "COUNTRY")
+    private String country;
+    @Column(name = "EMAIL_ADDRESS")
+    private String emailAddress;
+    @Column(name = "VAT_REGISTRATION_NUMBER")
+    private String vatRegistrationNumber;
+    @Column(name = "TAX_REGISTRATION_NUMBER")
+    private String taxRegistrationNumber;
+    @Column(name = "DIRECTOR_NAME")
+    private String directorName;
+    @Column(name = "DIRECTOR_DETAILS")
+    private String directorDetails;
+    @Column(name = "SPONSOR_NAME")
+    private String sponsorName;
+    @Column(name = "SPONSOR_DETAILS")
+    private String sponsorDetails;
+    @Column(name = "STATUS")
+    private String status;
+
+}
---
+++ src/main/java/org/trishanku/oa/admin/model/CustomerDTO.java
@@ -0,0 +1,33 @@
+package org.trishanku.oa.admin.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class CustomerDTO {
+
+    private UUID uuid;
+    private String customerId;
+    private String name;
+    private String addressLine1;
+    private String addressLine2;
+    private String addressLine3;
+    private String poBox;
+    private String country;
+    private String emailAddress;
+    private String vatRegistrationNumber;
+    private String taxRegistrationNumber;
+    private String directorName;
+    private String directorDetails;
+    private String sponsorName;
+    private String sponsorDetails;
+    private String status;
+
+}
