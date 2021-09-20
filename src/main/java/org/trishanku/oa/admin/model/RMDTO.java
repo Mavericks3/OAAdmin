d24ce546d46a01ac9f0cf5e4eb2a1c9f9dc0458a
Ravikanth <ravikanthm@gmail.com>
2021-09-20 13:07:50 +0400

    Create RM DTO closes #50

---
+++ src/main/java/org/trishanku/oa/admin/model/RMDTO.java
@@ -0,0 +1,23 @@
+package org.trishanku.oa.admin.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class RMDTO {
+
+
+    private String rmId;
+    private String name;
+    private String emailAddress;
+    private Date joiningDate;
+    private Date validDate;
+    private Date expiryDate;
+}
