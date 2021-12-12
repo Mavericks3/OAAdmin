fb5851ec48c34072f5cb8f166b8d9bb9f4bde12d
mamillapalli <ravikanthm@gmail.com>
2021-12-13 01:09:07 +0400

    split logs to multiple files closes #132

--- src/main/java/org/trishanku/oa/admin/aspect/LoggingAspect.java
+++ src/main/java/org/trishanku/oa/admin/aspect/LoggingAspect.java
@@ -8,6 +8,8 @@ import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -23,9 +25,12 @@ import java.util.UUID;
 
 @Aspect
 @Component
-@Slf4j
+
 public class LoggingAspect {
 
+    Logger controllerLog = LoggerFactory.getLogger("ControllerLog");
+    Logger serviceLog = LoggerFactory.getLogger("ServiceLog");
+    Logger repositoryLog = LoggerFactory.getLogger("RepositoryLog");
 
     @Autowired
     AuditRepository auditRepository;
@@ -40,20 +45,28 @@ public class LoggingAspect {
     /**
      * Pointcut that matches all repositories, services and Web REST endpoints.
      */
-    @Pointcut("within(@org.springframework.stereotype.Repository *)" +
-            " || within(@org.springframework.stereotype.Service *)" +
-            " || within(@org.springframework.web.bind.annotation.RestController *)")
-    public void springBeanPointcut() {
-        // Method is empty as this is just a Pointcut, the implementations are in the advices.
-    }
+//    @Pointcut("within(@org.springframework.stereotype.Repository *)" +
+//            " || within(@org.springframework.stereotype.Service *)" +
+//            " || within(@org.springframework.web.bind.annotation.RestController *)")
+//    public void springBeanPointcut() {
+//        // Method is empty as this is just a Pointcut, the implementations are in the advices.
+//    }
 
     /**
      * Pointcut that matches all Spring beans in the application's main packages.
      */
-    @Pointcut("within(org.trishanku.oa.admin..*)" +
-            " || within(org.trishanku.oa.admin.service..*)" +
-            " || within(org.trishanku.oa.admin.controller..*)")
-    public void applicationPackagePointcut() {
+    @Pointcut("within(org.trishanku.oa.admin.repository..*)")
+    public void applicationRepositoryPointcut() {
+        // Method is empty as this is just a Pointcut, the implementations are in the advices.
+    }
+
+    @Pointcut("within(org.trishanku.oa.admin.service..*)")
+    public void applicationServicePointcut() {
+        // Method is empty as this is just a Pointcut, the implementations are in the advices.
+    }
+
+    @Pointcut("within(org.trishanku.oa.admin.controller..*)")
+    public void applicationControllerPointcut() {
         // Method is empty as this is just a Pointcut, the implementations are in the advices.
     }
 
@@ -61,11 +74,23 @@ public class LoggingAspect {
      * Advice that logs methods throwing exceptions.
      *
      * @param joinPoint join point for advice
-     * @param e exception
+     * @param e         exception
      */
-    @AfterThrowing(pointcut = "applicationPackagePointcut() && springBeanPointcut()", throwing = "e")
-    public void logAfterThrowing(JoinPoint joinPoint, Throwable e) {
-        log.error("Exception in {}.{}() with cause = {}", joinPoint.getSignature().getDeclaringTypeName(),
+    @AfterThrowing(pointcut = "applicationRepositoryPointcut()", throwing = "e")
+    public void logAfterThrowingForRepositories(JoinPoint joinPoint, Throwable e) {
+        repositoryLog.error("Exception in {}.{}() with cause = {}", joinPoint.getSignature().getDeclaringTypeName(),
+                joinPoint.getSignature().getName(), e.getCause() != null ? e.getCause() : "NULL");
+    }
+
+    @AfterThrowing(pointcut = "applicationServicePointcut()", throwing = "e")
+    public void logAfterThrowingForServices(JoinPoint joinPoint, Throwable e) {
+        serviceLog.error("Exception in {}.{}() with cause = {}", joinPoint.getSignature().getDeclaringTypeName(),
+                joinPoint.getSignature().getName(), e.getCause() != null ? e.getCause() : "NULL");
+    }
+
+    @AfterThrowing(pointcut = "applicationControllerPointcut()", throwing = "e")
+    public void logAfterThrowingForControllers(JoinPoint joinPoint, Throwable e) {
+        controllerLog.error("Exception in {}.{}() with cause = {}", joinPoint.getSignature().getDeclaringTypeName(),
                 joinPoint.getSignature().getName(), e.getCause() != null ? e.getCause() : "NULL");
     }
 
@@ -81,40 +106,59 @@ public class LoggingAspect {
      * @return result
      * @throws Throwable throws IllegalArgumentException
      */
-    @Around("applicationPackagePointcut() && springBeanPointcut()")
-    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
-        if (log.isDebugEnabled()) {
-            log.debug("Enter: {}.{}() with argument[s] = {}", joinPoint.getSignature().getDeclaringTypeName(),
-                    joinPoint.getSignature().getName(), Arrays.toString(joinPoint.getArgs()));
-        }
+    @Around("applicationRepositoryPointcut()")
+    public Object logAroundForRepositories(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        repositoryLog.debug("Enter: {}.{}() with argument[s] = {}", joinPoint.getSignature().getDeclaringTypeName(),
+                joinPoint.getSignature().getName(), Arrays.toString(joinPoint.getArgs()));
+
         try {
             Object result = joinPoint.proceed();
-            if (log.isDebugEnabled()) {
-                log.debug("Exit: {}.{}() with result = {}", joinPoint.getSignature().getDeclaringTypeName(),
-                        joinPoint.getSignature().getName(), result);
-            }
+
+            repositoryLog.debug("Exit: {}.{}() with result = {}", joinPoint.getSignature().getDeclaringTypeName(),
+                    joinPoint.getSignature().getName(), result);
+
             return result;
         } catch (IllegalArgumentException e) {
-            log.error("Illegal argument: {} in {}.{}()", Arrays.toString(joinPoint.getArgs()),
+            repositoryLog.error("Illegal argument: {} in {}.{}()", Arrays.toString(joinPoint.getArgs()),
                     joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName());
             throw e;
         }
     }
 
+    @Around("applicationServicePointcut()")
+    public Object logAroundForservices(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        serviceLog.debug("Enter: {}.{}() with argument[s] = {}", joinPoint.getSignature().getDeclaringTypeName(),
+                joinPoint.getSignature().getName(), Arrays.toString(joinPoint.getArgs()));
+
+        try {
+            Object result = joinPoint.proceed();
+
+            serviceLog.debug("Exit: {}.{}() with result = {}", joinPoint.getSignature().getDeclaringTypeName(),
+                    joinPoint.getSignature().getName(), result);
+
+            return result;
+        } catch (IllegalArgumentException e) {
+            serviceLog.error("Illegal argument: {} in {}.{}()", Arrays.toString(joinPoint.getArgs()),
+                    joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName());
+            throw e;
+        }
+    }
 
     @Around("controllerPointcut()")
     public Object logControllerAccess(ProceedingJoinPoint joinPoint) throws Throwable {
 
-        System.out.println("testing the logging aspect");
+        //System.out.println("testing the logging aspect");
 
-        log.info("Enter: {}.{}() with argument[s] = {}", joinPoint.getSignature().getDeclaringTypeName(),
+        controllerLog.info("Enter: {}.{}() with argument[s] = {}", joinPoint.getSignature().getDeclaringTypeName(),
                 joinPoint.getSignature().getName(), Arrays.toString(joinPoint.getArgs()));
 
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
 
 
         // TO BE REPLACED WITH THE DETAILS RETRIEVED FROM JWT
-        auditRepository.save(Audit.builder().accessedBy(jwtUtil.extractUsernameFromRequest()).accessedResource(joinPoint.getSignature().getDeclaringTypeName()+"-->"+joinPoint.getSignature().getName()).eventAt(new Date())
+        auditRepository.save(Audit.builder().accessedBy(jwtUtil.extractUsernameFromRequest()).accessedResource(joinPoint.getSignature().getDeclaringTypeName() + "-->" + joinPoint.getSignature().getName()).eventAt(new Date())
                 .inputParameters(Arrays.toString(joinPoint.getArgs())).uuid(UUID.randomUUID()).eventAction("Entry").remoteAddress(request.getRemoteAddr()).remoteHost(request.getRemoteHost())
                 .remoteUser(request.getRemoteUser()).build());
 
@@ -122,21 +166,20 @@ public class LoggingAspect {
             Object result = joinPoint.proceed();
 
 
-
-            log.info("Exit: {}.{}() with result = {}", joinPoint.getSignature().getDeclaringTypeName(),
+            controllerLog.info("Exit: {}.{}() with result = {}", joinPoint.getSignature().getDeclaringTypeName(),
                     joinPoint.getSignature().getName(), result);
 
-            auditRepository.save(Audit.builder().accessedBy(jwtUtil.extractUsernameFromRequest()).accessedResource(joinPoint.getSignature().getDeclaringTypeName()+"-->"+joinPoint.getSignature().getName()).eventAt(new Date())
+            auditRepository.save(Audit.builder().accessedBy(jwtUtil.extractUsernameFromRequest()).accessedResource(joinPoint.getSignature().getDeclaringTypeName() + "-->" + joinPoint.getSignature().getName()).eventAt(new Date())
                     .returnedResult(objectMapper.writeValueAsString(result)).uuid(UUID.randomUUID()).eventAction("Exit")
                     .remoteAddress(request.getRemoteAddr()).remoteHost(request.getRemoteHost())
                     .remoteUser(request.getRemoteUser()).build());
 
             return result;
         } catch (IllegalArgumentException e) {
-            log.error("Illegal argument: {} in {}.{}()", Arrays.toString(joinPoint.getArgs()),
+            controllerLog.error("Illegal argument: {} in {}.{}()", Arrays.toString(joinPoint.getArgs()),
                     joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName());
 
-            auditRepository.save(Audit.builder().accessedBy(jwtUtil.extractUsernameFromRequest()).accessedResource(joinPoint.getSignature().getDeclaringTypeName()+"-->"+joinPoint.getSignature().getName()).eventAt(new Date())
+            auditRepository.save(Audit.builder().accessedBy(jwtUtil.extractUsernameFromRequest()).accessedResource(joinPoint.getSignature().getDeclaringTypeName() + "-->" + joinPoint.getSignature().getName()).eventAt(new Date())
                     .inputParameters(Arrays.toString(joinPoint.getArgs())).uuid(UUID.randomUUID()).eventAction("Illegal Argument")
                     .remoteAddress(request.getRemoteAddr()).remoteHost(request.getRemoteHost())
                     .remoteUser(request.getRemoteUser()).build());
